51095696481ccbd61d6ec18bf3940f8f18e0a6d5
neil <bn0010100@outlook.com>
2023-01-29 22:08:14 +0800

    ajust to juejin config

--- README.md
+++ README.md
@@ -2,3 +2,17 @@
 
 A starter template for [LazyVim](https://github.com/LazyVim/LazyVim).
 Refer to the [documentation](https://lazyvim.github.io/installation) to get started.
+
+## TODO
+- [x] nvim-tree
+- [x] rigrep
+- [x] fd-find
+- [x] wsl(no windows env)
+- [ ] autosave
+- [ ] lazygit
+- [ ] markdown autocomplete
+- [ ] why no auto format lua in the company
+- [ ] better key bindings
+- [ ] whichkey how to use, the theory
+- [ ] code jump back?
+
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local map = vim.api.nvim_set_keymap
+local opt = { noremap = true, silent = true }
+map("n", "<A-m>", ":NvimTreeToggle<CR>", opt)
+map("n", "<C-w>", ":Bdelete!<CR>", opt)
+-- Telescope
+-- 查找文件
+map("n", "<C-p>", ":Telescope find_files<CR>", opt)
+-- 全局搜索
+map("n", "<C-f>", ":Telescope live_grep<CR>", opt)
